a9555b986052550c00d694fd4e4135d53c9a3ff7
vamshionrails <vamshionrails@server.local>
2013-04-09 21:23:45 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jobportal::Application.config.secret_key_base = '9d2ebbbd6b01ee0d2a954c7d91fffcce7e74e3dbf9d94523c5c6d1cb0efcc3776aebf68386e341ca6eea7dbda803d8a448114f08268d204ec78922c26b65d4a4'
